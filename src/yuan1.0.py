6ac1773d6d2717a3397269bfa1a51c4b2c94d6d3
renyi <renyi1006@gmail.com>
2022-10-07 23:22:40 +0800

    feat: finish modularization of yuan1.0 api

--- src/yuan1.0.py
+++ src/yuan1.0.py
@@ -1,3 +1,22 @@
 import requests
-res = requests.get('http://221.194.179.88:11016/?question=中国第一位女皇帝是谁&usrname=hacker_anonymous&phone=17812007230')
-print(res.text)
+import sys
+import tomli
+
+
+def main():
+    q = sys.argv[1] if len(sys.argv) > 1 else '中国第一位女皇帝是谁？'
+    print('question is: {}'.format(q))
+    with open('../credentials/yuan1.0.toml', mode='rb') as f:
+        config = tomli.load(f)
+    username = config['api']['username']
+    telephone = config['api']['telephone']
+    print(yuan1_0_ask(q, username, telephone))
+
+
+def yuan1_0_ask(q: str, username, telephone):
+    res = requests.get('http://221.194.179.88:11016/', params={'question': q, 'usrname': username, 'phone': telephone})
+    return res.text
+
+
+if __name__ == '__main__':
+    main()
