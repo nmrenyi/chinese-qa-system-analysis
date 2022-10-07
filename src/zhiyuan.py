bc6cc5a8a4c32c7cceb9b6fb507d4e8ea3e68aa0
renyi <renyi1006@gmail.com>
2022-10-07 22:45:31 +0800

    feat: change ability_type and engine type for QA use

--- src/zhiyuan.py
+++ src/zhiyuan.py
@@ -1,40 +1,29 @@
 # api from https://open.wudaoai.com/trialcenter
-from wudaoai.api_request import executeEngine, getToken # install with `pip install wudaoai`
+from wudaoai.api_request import executeEngine, getToken  # install with `pip install wudaoai`
 
 '''
    获取接口鉴权token，请求参数 api_Key、 public_key 开放平台个人中心API Keys中获取。
    token有效期为8小时，建议放入缓存中不必每次请求获取，缓存失效时间设置小于8小时。
 '''
 # 接口API KEY
-api_key = "your api key"
+api_key = "626ca361df7e4a4b9fc92ded56a67d93"
 # 公钥
-public_key = "your public key"
+public_key = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAOoF6XHqAlnKvMi4UiCPolnaG8okQ3l0o34gWmbIl1ABdEpUX3sDpwXO6e4SAejZEE48WTLXmhWkfRAgr2gm1h8CAwEAAQ=="
 # 鉴权token
 token = getToken(api_key, public_key)
 # 能力类型
-ability_type = "articleTitle"
+ability_type = "question_answer"
 # 引擎类型
-engine_type = "title-creation"
+engine_type = "txl-general-engine-v1"
 
 data = {
-    "prompt": "新闻 炸裂"  # 内容输入（必填）取值范围1～200
+    "topP": 1,  # 核采样（必填）取值范围0～1
+    "topK": 3,  # 采样（必填）取值范围0～10
+    "temperature": 1,  # 概率分布调节（必填)取值范围0.5～1
+    "presencePenalty": 1,  # 重复词去除（必填)取值范围0～20
+    "frequencyPenalty": 1,  # 重复惩罚参数（必填)取值范围0～3
+    "generatedLength": 128,  # 文本生成最大长度（必填)取值范围1～512
+    "prompt": "世界的终极意义是什么？",  # 标题关键词（必填）取值范围1～200
 }
 resp = executeEngine(ability_type, engine_type, token, data)
 
-'''
-    {
-      "code": 200,  # 请求响应状态 200 为业务处理成功
-      "msg": "成功",  # 错误信息
-      "data": {
-               "prompt": "新闻  炸裂",  # 用户输入项 
-               "promptDesc": None, 
-               "outputText": "!炸裂!炸裂!《炸裂》终于定档了,一起来看下吧...",  # 平台输出
-               "inputTokenNum": 9,  # 输入词块数
-               "outputTokenNum": 19, # 输出词块数
-               "totalTokenNum": 28,  # 总词块数
-               "taskOrderNo": "1000651508360039046451200", # 任务订单号
-               "taskStatus":"SUCCESS"
-               }, 
-      "success": True
-    }
-'''
