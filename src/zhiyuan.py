fed9eafc7653c92ece5ecc4fe8e698a028b05641
renyi <renyi1006@gmail.com>
2022-10-08 23:44:52 +0800

    refactor: refactor zhiyuan api to fit main entrance call

--- src/zhiyuan.py
+++ src/zhiyuan.py
@@ -20,10 +20,10 @@ def read_zhiyuan_config():
     # 获取接口鉴权token，请求参数 api_Key、 public_key 开放平台个人中心API Keys中获取。
     # token有效期为8小时，建议放入缓存中不必每次请求获取，缓存失效时间设置小于8小时。
     token = getToken(api_key, public_key)
-    return token
+    return (token, )
 
 
-def zhiyuan_ask(q: str, token):
+def zhiyuan_ask(q: str, token: tuple):
     # 能力类型
     ability_type = "question_answer"
     # 引擎类型
@@ -38,7 +38,7 @@ def zhiyuan_ask(q: str, token):
         "generatedLength": 128,  # 文本生成最大长度（必填)取值范围1～512
         "prompt": q,  # 标题关键词（必填）取值范围1～200
     }
-    resp = executeEngine(ability_type, engine_type, token, data)
+    resp = executeEngine(ability_type, engine_type, token[0], data)
     res = resp['data']['outputText'].strip()
     return res
 
