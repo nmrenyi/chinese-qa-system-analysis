0c12d60fd0b7479135cff4da77349f16887ebbe5
renyi <renyi1006@gmail.com>
2022-10-09 00:17:10 +0800

    feat: finish input and output file specification

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import argparse
 import sys
 
 import pandas as pd
+from tqdm import tqdm
+
 from baidu import read_baidu_config, baidu_ask
 from zhiyuan import read_zhiyuan_config, zhiyuan_ask
 from yuan1_0 import read_yuan1_0_config, yuan1_0_ask
@@ -10,16 +12,19 @@ from yuan1_0 import read_yuan1_0_config, yuan1_0_ask
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--engine', type=str, default='baidu', help='baidu, yuan1.0, zhiyuan')
-    parser.add_argument('--question', type=str, default='中国的第一位女皇帝是谁', help='question')
+    parser.add_argument('--in_file', type=str, default='../input/in1.csv', help='input file')
+    parser.add_argument('--out_file', type=str, default='../output/out1.csv', help='output file')
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    q = args.question
     engine = args.engine
+    in_file = args.in_file
+    out_file = args.out_file
     print('engine is: {}'.format(engine))
-    print('question is: {}'.format(q))
+    print('input file is: {}'.format(in_file))
+    print('output file is: {}'.format(out_file))
 
     engine_dict = {'baidu': read_baidu_config, 'yuan1.0': read_yuan1_0_config, 'zhiyuan': read_zhiyuan_config}
     ask_dict = {'baidu': baidu_ask, 'yuan1.0': yuan1_0_ask, 'zhiyuan': zhiyuan_ask}
@@ -27,8 +32,17 @@ def main():
     config = engine_dict[engine]()
     print('config is: {}'.format(config))
 
-    res = ask_dict[engine](q, *config)
-    print(res)
+    df = pd.read_csv(in_file, sep='\t')
+    ans_list = []
+    for q in tqdm(df['question']):
+        print('question is: {}'.format(q))
+        ans = ask_dict[engine](q, *config)
+        print('answer is: {}'.format(ans))
+        ans_list.append(ans)
+
+    df['answer'] = ans_list
+    df.to_csv(out_file, sep='\t', index=False)
+    print('file saved to: {}'.format(out_file))
 
 
 if __name__ == '__main__':
