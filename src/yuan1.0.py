2278e34c87e492acbbcc6a69f9a263bb2afaa5a5
renyi <renyi1006@gmail.com>
2022-10-08 23:26:29 +0800

    refactor: isolate config reading from asking

--- src/yuan1.0.py
+++ src/yuan1.0.py
@@ -6,11 +6,16 @@ import tomli
 def main():
     q = sys.argv[1] if len(sys.argv) > 1 else '中国第一位女皇帝是谁？'
     print('question is: {}'.format(q))
+    username, telephone = read_yuan1_0_config()
+    print(yuan1_0_ask(q, username, telephone))
+
+
+def read_yuan1_0_config():
     with open('../credentials/yuan1.0.toml', mode='rb') as f:
         config = tomli.load(f)
     username = config['api']['username']
     telephone = config['api']['telephone']
-    print(yuan1_0_ask(q, username, telephone))
+    return username, telephone
 
 
 def yuan1_0_ask(q: str, username, telephone):
